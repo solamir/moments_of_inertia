63a791317cbc9ebc811f1f8f4928c2b1a020661e
Mikhail Koverda <m.kov@pm.me>
2019-02-23 21:49:43 +0300

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
-# moments_of_inertia
-The program for calculating the moments of inertia of the molecule using xyz file as input data 
+# Moment of inertia
+
+The program calculates the minimum (*Ix*) and maximum (*Iz*) moments of inertia of the molecule using atomic coordinates from the [XYZ file](https://en.wikipedia.org/wiki/XYZ_file_format).
+
+## System requirements
+
+For normal operation of the program, at least 2Gb of RAM is required. However, when calculating large molecules (more than 30 atoms), the recommended amount of RAM is 8 Gb.
+
+## Software requirements
+
+To run the program, you need a Perl interpreter of version not lower than 5.10 and a shell to start the program. The program can run as a Unix command shell on the Linux operating system, in which the Perl interpreter is usually present with the system, and on the Microsoft Windows operating system using the Windows Power Shell and the installed Perl interpreter, for example, [Strawberry Perl](http://strawberryperl.com/).
+
+## Usage
+
+To use the program, run it in the shell with the location of the XYZ file, for example:
+
+```
+$ ./moments_of_inertia.pl filename.xyz
+```
+
+The program will create a text file *filename.xyz.txt* with the results of the calculation in the directory where the file *filename.xyz* is located. Examples of source XYZ files can be found in the *Examples* folder. For example, launching the program with the file *Pyridine.xyz* will create the file *Pyridine.xyz.txt* with the results of the calculation:
+
+```
+Calculation for data from file ./Pyridine.xyz
+The calculation with step of the directing vector is 0.01
+Coordinates of the center of gravity: (-0.18023; 0.36095; 0.29286)
+Moment of inertia Ix is: 83.776 Da*(Å^2)
+Moment of inertia Iz is: 170.861 Da*(Å^2)
+```
+
+You can set the parameters of the program using the following startup keys:
+
+**-v** - Show program version and exit.
+
+**-a[1-5]** - Accuracy counting. Sets the interval of movement of the directing vector of the axis of rotation in space. The smaller the interval, the more accurate and longer the calculation takes place (see the principle of operation): **-a1** – 0.1; **-a2** – 0.05; **-a3** – 0.01; **-a4** – 0.005;**-a5** – 0.001. The default key is **-a3**.
+
+**-o[1-5]** - The number of significant figures to which the value of the moments of inertia will be rounded (in units of Da * Å2). The default key is **-o3**.
+
+**-d** - Display coordinates of axis directing vectors with minimum and maximum inertia moments.
+
+For example, the result of the calculation for the pentane molecule using the XYZ file from the *Examples* folder with the keys will look like this:
+
+```
+Calculation for data from file ./Pentane.xyz
+The calculation with step of the directing vector is 0.01
+Coordinates of the center of gravity: (3.13712; 0.78884; -0.90687)
+Moment of inertia Ix is: 29.987 Da*(Å^2)
+Moment of inertia Iz is: 269.277 Da*(Å^2)
+```
+
+And when you run a program with keys
+
+```
+$ ./moments_of_inertia.pl -a2 -o4 -d ./Pentane.xyz
+```
+
+The result will look like this:
+
+```
+Calculation for data from file ../Pentane.xyz
+The calculation with step of the directing vector is 0.05
+Coordinates of the center of gravity: (3.13712; 0.78884; -0.90687)
+Moment of inertia Ix is: 29.9888 Da*(Å^2)
+Moment of inertia Iz is: 269.2769 Da*(Å^2)
+Coordinates of the directing vector a_x = {i, j, k} is: 0.55 0.25 -0.3
+Coordinates of the directing vector a_z = {i, j, k} is: 0 -0.85 -0.7
+```
+
+*Note!*
+
+Usually, 3D-visualization programs of molecules store XYZ files with a coordinate grid, in which 1 Å is taken as a unit of length. If this condition is not met, the program will calculate the moments of inertia incorrectly in absolute value, but in the correct ratios.
+
+## Contacts
+
+With corrections, questions, comments and suggestions, please email:
+
+* Mikhail Koverda m.kov@pm.me
+* Eugene N. Ofitserov ofitser@mail.ru
+
+## License
+
+This program is licensed under the GNU General Public License v3.0.
+
+## Principle of operation
+
+The XYZ file is a text file in which for each atom of the molecule its coordinates are indicated in the Cartesian system, for example, the XYZ file for pentane contains the following information:
+
+```
+17
+
+C      0.88843      0.03444      0.00635
+C      2.40855      0.05283     -0.01558
+C      2.94163      0.96482     -1.12020
+C      4.46987      0.98077     -1.13892
+C      5.00405      1.88707     -2.23658
+H      0.52876     -0.62374      0.80344
+H      0.48677     -0.33185     -0.94384
+H      0.48652      1.03669      0.18608
+H      2.77834     -0.96831     -0.16472
+H      2.77808      0.39277      0.95890
+H      2.56526      1.98395     -0.96898
+H      2.56572      0.62305     -2.09233
+H      4.85117     -0.03520     -1.29421
+H      4.85071      1.32591     -0.17064
+H      4.66744      1.54992     -3.22215
+H      6.09853      1.88355     -2.23189
+H      4.66701      2.91854     -2.09234
+```
+
+The program works according to the following algorithm:
+
+1. Data from the file is read and the symbol of the element is replaced with the value of its atomic mass in Daltons.
+2. Calculate the coordinates of the center of gravity of the molecule.
+3. A straight line is set that passes through the center of gravity of the molecule and has an arbitrary direction vector, for example ***a*** = (0, -1, -1).
+4. The coordinates of the direction vector iteratively change with a certain interval of movement (by specifying the **–a** key) so that the direction of the vector corresponds to all possible directions of the axis of rotation in space, for example, ***ax*** =[0,1], ***ay*** = [-1,1], ***az*** = [-1,1].
+5. For each position of the axis in space, the moment of inertia is calculated relative to this axis, considering the distance of each atom to this axis and stored in memory.
+6. After the calculation of the moments of inertia for all positions of the axis, the program selects the largest and smallest moments of inertia and returns their values in units Da*Å^2, as well as the coordinates of the direction vectors ***a*** and ***b*** for these axes.
+7. The resulting axes are always perpendicular to each other, as can be easily seen by calculating the scalar product of the directing vectors for these axes.
---
+++ moments_of_inertia.pl
@@ -0,0 +1,333 @@
+#!/usr/bin/perl
+
+# ===============================================================================================
+# This program is designed to calculate the minimum and maximum moments of inertia of a molecule,
+# given by the coordinates of atoms in three-dimensional space as part of the structure-property
+# studies carried out at the D. Mendeleev University of Chemical Technology of Russia. For all
+# questions write to Mikhail Koverda (m.kov@pm.me)
+# ===============================================================================================
+
+use 5.010;
+use warnings;
+use strict;
+
+# Atomic mass table of chemical elements in Daltons
+my %atomic_masses = (
+    "H" => 1.008,
+    "B" => 10.810,
+    "C" => 12.011,
+    "N" => 14.007,
+    "O" => 15.999,
+    "F" => 18.998,
+    "Na" => 22.990,
+    "Mg" => 24.305,
+    "Al" => 26.982,
+    "Si" => 28.085,
+    "P" => 30.974,
+    "S" => 32.060,
+    "Cl" => 35.450,
+    "Br" => 79.904,
+    "I" => 126.900,
+);
+
+# Checking keys and setting appropriate values:
+# -v - display the program version
+# -a[1-5] - accuracy of the calculation of the moments of inertia (default is -a3)
+# -o[1-5] - number of significant figures in the output result
+# -d - display the coordinates of the directing vectors
+my $version = "0.1";
+my $accuracy = undef;
+my $significant_figures = undef;
+my $direct_vector_output = undef;
+
+foreach (@ARGV) {
+   if ($_ eq "-v") {
+        print "This is program version $version\n";
+        exit;
+    } 
+}
+
+foreach (@ARGV) {
+    # Accuracy of the calculation
+    if ($_ eq "-a1") {
+        $accuracy = 0.1;
+        last;
+    }
+    elsif ($_ eq "-a2") {
+        $accuracy = 0.05;
+        last;
+    }
+    elsif ($_ eq "-a3") {
+        $accuracy = 0.01;
+        last;
+    }
+    elsif ($_ eq "-a4") {
+        $accuracy = 0.005;
+        last;
+    }
+    elsif ($_ eq "-a5") {
+        $accuracy = 0.001;
+        last;
+    }
+    else {
+        $accuracy = 0.01;
+    }
+}
+
+foreach (@ARGV) {
+    # Number of significant figures in the output result
+    if ($_ eq "-o1") {
+        $significant_figures = 1;
+        last;
+    }
+    elsif ($_ eq "-o2") {
+        $significant_figures = 2;
+        last;
+    }
+    elsif ($_ eq "-o3") {
+        $significant_figures = 3;
+        last;
+    }
+    elsif ($_ eq "-o4") {
+        $significant_figures = 4;
+        last;
+    }
+    elsif ($_ eq "-o5") {
+        $significant_figures = 5;
+        last;
+    }
+    else {
+        $significant_figures = 3;
+    }
+}
+
+foreach (@ARGV) {
+    if ($_ eq "-d") {
+        $direct_vector_output = "true";
+        last;
+    }
+}
+
+# Opening a file and reading information
+my $filename = "$ARGV[-1]";
+open(DATA, "$filename") or die "Could not open file '$filename' $!";
+chomp(my @xyz = <DATA>);
+
+# The number N of atoms in a molecule
+my $N = shift @xyz;
+
+# Delete comments
+shift @xyz;
+
+# Displays preliminary information about the calculation
+open OUTPUT, ">>", "${filename}.txt";
+print OUTPUT "\n";
+print OUTPUT "Calculation for data from file $ARGV[-1]\n";
+print OUTPUT "The calculation with step of the directing vector is $accuracy\n";
+
+# Delete first and last whitespace
+foreach (@xyz) {
+    s/^\s+|\s+$//g;
+}
+
+# Replacing the character of an element in a string with the value of its atomic mass
+foreach (@xyz) {
+    my @atom_data = split;
+    if ($atom_data[0] =~ /^H$/) {
+        $atom_data[0] = $atomic_masses{"H"};        
+    }
+    if ($atom_data[0] =~ /^B$/) {
+        $atom_data[0] = $atomic_masses{"B"};      
+    }
+    if ($atom_data[0] =~ /^C$/) {
+        $atom_data[0] = $atomic_masses{"C"};      
+    }
+    if ($atom_data[0] =~ /^N$/) {
+        $atom_data[0] = $atomic_masses{"N"};      
+    }
+    if ($atom_data[0] =~ /^O$/) {
+        $atom_data[0] = $atomic_masses{"O"};      
+    }
+    if ($atom_data[0] =~ /^F$/) {
+        $atom_data[0] = $atomic_masses{"F"};      
+    }
+    if ($atom_data[0] =~ /^Na$/) {
+        $atom_data[0] = $atomic_masses{"Na"};      
+    }
+    if ($atom_data[0] =~ /^Mg$/) {
+        $atom_data[0] = $atomic_masses{"Mg"};      
+    }
+    if ($atom_data[0] =~ /^Al$/) {
+        $atom_data[0] = $atomic_masses{"Al"};      
+    }
+    if ($atom_data[0] =~ /^Si$/) {
+        $atom_data[0] = $atomic_masses{"Si"};      
+    }
+    if ($atom_data[0] =~ /^P$/) {
+        $atom_data[0] = $atomic_masses{"P"};      
+    }
+    if ($atom_data[0] =~ /^S$/) {
+        $atom_data[0] = $atomic_masses{"S"};      
+    }
+    if ($atom_data[0] =~ /^Cl$/) {
+        $atom_data[0] = $atomic_masses{"Cl"};      
+    }
+    if ($atom_data[0] =~ /^Br$/) {
+        $atom_data[0] = $atomic_masses{"Br"};      
+    }
+    if ($atom_data[0] =~ /^I$/) {
+        $atom_data[0] = $atomic_masses{"I"};      
+    }                        
+    $_ = "@atom_data";
+}
+
+# Finding the coordinates of the center of mass
+my $sum_Xm = undef;
+my $sum_Ym = undef;
+my $sum_Zm = undef;
+my $sum_m = undef;
+
+foreach (@xyz) {
+    my @atom_data = split;
+    $sum_Xm += $atom_data[0] * $atom_data[1];
+    $sum_Ym += $atom_data[0] * $atom_data[2];
+    $sum_Zm += $atom_data[0] * $atom_data[3];
+    $sum_m += $atom_data[0];
+}
+
+my $X_c = $sum_Xm / $sum_m;
+my $Y_c = $sum_Ym / $sum_m;
+my $Z_c = $sum_Zm / $sum_m;
+
+# Rounding the coordinates of the center of masses and their output
+my $X_c_rounded = sprintf("%.5f", $X_c);
+my $Y_c_rounded = sprintf("%.5f", $Y_c);
+my $Z_c_rounded = sprintf("%.5f", $Z_c);
+
+print OUTPUT "Coordinates of the center of gravity: ($X_c_rounded; $Y_c_rounded; $Z_c_rounded)\n";
+
+# The search of the moments of inertia with relative to the various lines passing through the center of masses
+# @I is array of inertia moments, and @XaYaZa is array of coordinates of the directing vectors of the lines
+# corresponding to this moment of inertia. The lines take all the spatial directions and for each straight line
+# the moment of inertia of the molecule is calculated relative to this line.
+my @I = undef;
+my @XaYaZa = undef;
+
+for (my $X_a = 0; $X_a <= 1; $X_a = $X_a + $accuracy) {
+    for (my $Y_a = -1; $Y_a <= 1; $Y_a = $Y_a + $accuracy) {
+        for (my $Z_a = -1; $Z_a <= 1; $Z_a = $Z_a + $accuracy) {
+            my $I = 0;
+            for (my $n = 1; $n <= $N; $n++) {
+                my $Ii = &mass($n) * (&distance(&coordinates($n, "x"), &coordinates($n, "y"), &coordinates($n, "z"), $X_a, $Y_a, $Z_a)) ** 2;
+                $I += $Ii;
+            }
+            push @I, $I;
+            push @XaYaZa, ("$X_a" . " " . "$Y_a" . " " . "$Z_a");
+        }
+    }
+}
+
+# Remove the first undef value from the arrays @I and @XaYaZa and find Ix, Iz and its directing vector сoordinates 
+shift @I;
+shift @XaYaZa;
+
+my $I_min = &min(@I);
+my $I_max = &max(@I);
+my $I_min_rounded = sprintf("%.${significant_figures}f", $I_min);
+my $I_max_rounded = sprintf("%.${significant_figures}f", $I_max);
+
+print OUTPUT "Moment of inertia Ix is: $I_min_rounded Da*(Å^2)\n";
+print OUTPUT "Moment of inertia Iz is: $I_max_rounded Da*(Å^2)\n";
+
+if ($direct_vector_output) {
+    my $XaYaZa_MIN = &return_XaYaZa_MIN($I_min);
+    my $XaYaZa_MAX = &return_XaYaZa_MAX($I_max);
+    print OUTPUT "Coordinates of the directing vector a_x = {i, j, k} is: $XaYaZa_MIN\n";
+    print OUTPUT "Coordinates of the directing vector a_z = {i, j, k} is: $XaYaZa_MAX\n";
+}
+
+close OUTPUT;
+
+# ======================================================================
+# =========================== Functions ================================
+# ======================================================================
+
+# The function returns the atomic mass of the element with the number n:
+# &mass(n)
+sub mass {
+    my $atom_data = $xyz[$_[0] - 1];
+    my @atom_data = split /\s+/, $atom_data;
+    return $atom_data[0];
+}
+
+# The function returns the coordinates of the atom with the number n:
+# &coordinates(n, ["x", "y", "z"])
+sub coordinates {
+    my @atom_data = split /\s+/, $xyz[$_[0] - 1];
+    if ($_[1] =~ /x/) {
+        return $atom_data[1];
+    }
+    if ($_[1] =~ /y/) {
+        return $atom_data[2];
+    }
+    if ($_[1] =~ /z/) {
+        return $atom_data[3];
+    }
+}
+
+# The function returns the distance between the atom and the line defined by the directing vector c = (X_a; Y_a; Z_a):
+# &distance(x, y, z, X_a, Y_a, Z_a)
+sub distance {
+    my $u_x = $X_c - $_[0];
+    my $u_y = $Y_c - $_[1];
+    my $u_z = $Z_c - $_[2];
+
+    my $i = $_[5] * $u_y - $_[4] * $u_z;
+    my $j = -1 * ($_[5] * $u_x - $_[3] * $u_z);
+    my $k = $_[4] * $u_x - $_[3] * $u_y;
+
+    my $distance = ($i ** 2 + $j ** 2 + $k ** 2) ** 0.5 / ($_[3] ** 2 + $_[4] ** 2 + $_[5] ** 2) ** 0.5;
+    return $distance;
+}
+
+# Functions to find the minimum or maximum value:
+# &min(@array)
+sub min {
+    my ($min, @vars) = @_;
+    for (@vars) {
+        $min = $_ if $_ < $min;
+    }
+    return $min;
+}
+
+sub max {
+    my ($max, @vars) = @_;
+    for (@vars) {
+        $max = $_ if $_ > $max;
+    }
+    return $max;
+}
+
+# The functions returns the value of the coordinates of the directing vector according to
+# the number of minimum or maximum moment of inertia in the corresponding array:
+# &return_XaYaZa_MIN($I_min)
+# &return_XaYaZa_MAX($I_max)
+sub return_XaYaZa_MIN {
+    my $count = 0;
+    foreach (@I) {
+        if ($_ =~ /$I_min/) {
+            return $XaYaZa[$count];
+        }
+        $count++;
+    }
+}
+
+sub return_XaYaZa_MAX {
+    my $count = 0;
+    foreach (@I) {
+        if ($_ =~ /$I_max/) {
+            return $XaYaZa[$count];
+        }
+        $count++;
+    }
+}
